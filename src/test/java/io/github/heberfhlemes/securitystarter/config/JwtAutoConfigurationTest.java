b0a7adfa71a173d397a6b927d9732450160e1af6
HeberFHLemes <hfhlemes@gmail.com>
2025-12-15 11:30:05 -0300

    test(auto-configuration): writing unit tests for JwtAUtoConfiguration class, like check if it does not override user provided beans, correctly register beans, fails when JwtProperties is not correctly set, etc.

---
+++ src/test/java/io/github/heberfhlemes/securitystarter/config/JwtAutoConfigurationTest.java
@@ -0,0 +1,106 @@
+package io.github.heberfhlemes.securitystarter.config;
+
+import io.github.heberfhlemes.securitystarter.application.ports.TokenProvider;
+import io.github.heberfhlemes.securitystarter.application.services.TokenAuthenticationService;
+import io.github.heberfhlemes.securitystarter.infrastructure.jwt.JwtTokenProvider;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.FilteredClassLoader;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JwtAutoConfigurationTest {
+
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(JwtAutoConfiguration.class));
+
+    private ApplicationContextRunner withValidJwtProperties() {
+        return contextRunner.withPropertyValues(
+                "jjwt.secret=example-of-long-jjwt-secret-in-properties",
+                "jjwt.expiration=PT1H"
+        );
+    }
+
+    @Test
+    void shouldNotLoadWhenSecurityIsMissing() {
+        new ApplicationContextRunner()
+                .withConfiguration(AutoConfigurations.of(JwtAutoConfiguration.class))
+                .withClassLoader(new FilteredClassLoader(SecurityFilterChain.class))
+                .run(context -> {
+                    assertThat(context).doesNotHaveBean(TokenProvider.class);
+                    assertThat(context).doesNotHaveBean(OncePerRequestFilter.class);
+                    assertThat(context).doesNotHaveBean(TokenAuthenticationService.class);
+                });
+    }
+
+    @Test
+    void shouldRegisterJwtBeans() {
+        withValidJwtProperties()
+                .withBean(
+                        UserDetailsService.class,
+                        () -> Mockito.mock(UserDetailsService.class)
+                )
+                .run(context -> {
+                    assertThat(context).hasSingleBean(TokenProvider.class);
+                    assertThat(context).hasSingleBean(OncePerRequestFilter.class);
+                    assertThat(context).hasSingleBean(TokenAuthenticationService.class);
+                    assertThat(context.getBean(TokenProvider.class))
+                            .isInstanceOf(JwtTokenProvider.class);
+        });
+    }
+
+    @Test
+    void shouldNotOverrideUserProvidedTokenProvider() {
+        TokenProvider userProvided = Mockito.mock(TokenProvider.class);
+
+        withValidJwtProperties()
+                .withBean(
+                        UserDetailsService.class,
+                        () -> Mockito.mock(UserDetailsService.class)
+                )
+                .withBean(TokenProvider.class, () -> userProvided)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(TokenProvider.class);
+                    assertThat(context.getBean(TokenProvider.class))
+                            .isSameAs(userProvided);
+                });
+    }
+
+    @Test
+    void failsWhenSecretIsMissing() {
+        contextRunner
+                .withPropertyValues("jjwt.expiration=PT1H")
+                .withBean(UserDetailsService.class, () -> Mockito.mock(UserDetailsService.class))
+                .run(context -> {
+                    Throwable failure = context.getStartupFailure();
+
+                    assertThat(failure)
+                            .isNotNull()
+                            .hasRootCauseInstanceOf(IllegalArgumentException.class);
+                });
+    }
+
+    @Test
+    void failsWhenJwtSecretIsTooShort() {
+        contextRunner
+                .withPropertyValues(
+                        "jjwt.secret=short-secret",
+                        "jjwt.expiration=PT1H"
+                )
+                .withBean(UserDetailsService.class, () -> Mockito.mock(UserDetailsService.class))
+                .run(context -> {
+                    Throwable failure = context.getStartupFailure();
+
+                    assertThat(failure)
+                            .hasRootCauseInstanceOf(IllegalArgumentException.class);
+                });
+    }
+
+}
